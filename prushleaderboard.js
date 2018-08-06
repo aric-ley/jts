95e040bc172110482d2b8638217e2ef2a6554aaf
aric-ley <drahcir.c@gmail.com>
2018-08-06 18:36:20 -0400

    Create prushleaderboard.js

---
+++ prushleaderboard.js
@@ -0,0 +1,9 @@
+<script type=text/javascript>
+var a = document.createElement('div'); a.id = '_1360941'; a.className = '_e9af13232';
+var b = document.currentScript; if (b.parentElement) b.parentElement.insertBefore(a, b);
+var c = document.getElementsByTagName('head')[0];
+var d = document.createElement('script'); d.type = "text/javascript"; d.src = "//prscripts.com/d/?resource=pubJS";
+if (!window.prpubappended && !window.prpubappendedlocal) {
+window.prpubappendedlocal = true; c.appendChild(d);
+}
+</script>
